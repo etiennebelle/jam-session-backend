5d1bc540e28ddd2495017ad905e9e91fa359681a
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-11-23 10:23:04 +0100

    host model

---
+++ models/Host.model.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const hostSchema = new Schema(
+  {
+    barName: {
+        type: String, 
+        trim: true, 
+        required: true,
+        unique: true
+    },
+    address: {
+        type: String, 
+        trim: true, 
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String, 
+        required: true, 
+        lowercase: true, 
+        trim: true
+    }, 
+    password: {
+        type: String, 
+        required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Host = model("Host", hostSchema);
+
+module.exports = Host;
