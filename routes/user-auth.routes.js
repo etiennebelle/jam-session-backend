5f14b41628417bc2dc0c9c09f0059f43c02d1d8c
Etienne Bellé <bl.etienne7@gmail.com>
2022-11-23 17:54:13 +0100

    create user middleware

--- routes/user-auth.routes.js
+++ routes/user-auth.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {genSaltSync, hashSync, compareSync} = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User.model');
+const { isAuthenticated } = require('../middleware/jwt user-middleware');
 
 const router = express.Router();
 
@@ -100,6 +101,9 @@ router.post('/login', async (req, res, next) => {
  
  
 // GET  /user-auth/verify
-// ...
+router.get('/verify', isAuthenticated, (req, res, next) => {
+    console.log(`req.payload`, req.payload);
+    res.status(200).json(req.payload);
+})
 
 module.exports = router;
