9ddb093ec94af47a0a82a24fc53584074688fc69
Etienne Bellé <bl.etienne7@gmail.com>
2022-11-23 09:54:57 +0100

    first pull

--- models/User.model.js
+++ models/User.model.js
@@ -1,22 +1,10 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    email: {
-      type: String,
-      required: [true, 'Email is required.'],
-      unique: true,
-      lowercase: true,
-      trim: true
-    },
-    password: {
-      type: String,
-      required: [true, 'Password is required.']
-    }
+    
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
     timestamps: true
   }
 );
