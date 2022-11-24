6c2e90f1500236c74a40d9048f0c8e3e7457d3e3
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-11-24 17:52:15 +0100

    create rout for jamsession post

---
+++ routes/host.routes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const JamSession = require("../models/JamSession.model");
+
+router.post("/create-jam-session", async (req, res) => {
+    const {jamSessionName, date, capacity, genre, description} = req.body;
+
+    try {
+        await JamSession.create({jamSessionName, date, capacity, genre, description})
+        res.status(201).json({message: "Jam Session created successfully"});
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+});
