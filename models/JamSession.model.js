e05c74d5d7a5a0ce074fcd7af46b9531026bea07
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-11-24 17:41:46 +0100

    create jam session model

---
+++ models/JamSession.model.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+const jamSessionSchema = new Schema(
+  {
+    jamSessionName: {
+        type: String, 
+        trim: true, 
+        required: true,
+    },
+    date: {
+        type: Date, 
+        required: true,
+    },
+    capacity: {
+        type: Number, 
+        required: true, 
+    }, 
+    genre: {
+        type: String, 
+        required: true
+    },
+    description: {
+        type: String, 
+        required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const JamSession = model("JamSession", jamSessionSchema);
+
+module.exports = Host;
