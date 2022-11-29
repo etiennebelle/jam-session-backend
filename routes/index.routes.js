6c00113a6423c0cd0776fff2775a4677f81a7a54
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-11-29 08:44:09 +0100

    add location public page

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,23 +4,26 @@ const Host = require('../models/Host.model');
 const User = require('../models/User.model');
 const { isAuthenticated } = require('../middleware/jwt user-middleware');
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
-});
-
 router.get("/events", async (req, res, next) => {
-  const events = await JamSession.find().populate('host');
-  res.json(events);
+  try {  
+    const events = await JamSession.find().populate('host');
+    res.status(200).json(events);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" })
+  }
 })
+
 
 router.get("/events/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
     const jamEvent = await JamSession.findById(id).populate('host').populate('players')
-    res.json(jamEvent);
+    res.status(200).json(jamEvent);
 
   } catch (error) {
     console.log(error);
+    res.status(500).json({ message: "Internal Server Error" })
   }
 })
 
@@ -35,6 +38,7 @@ router.put('/events/:id', isAuthenticated, async (req, res) => {
     res.status(200).json({ message: "Player added successfully" })
   } catch (error) {
     console.log(error);
+    res.status(500).json({ message: "Internal Server Error" })
   }
 })
 
@@ -48,7 +52,21 @@ router.delete('/events/:id', isAuthenticated, async (req, res) => {
     res.status(200).json({ message: "Player removed successfully" })
   } catch (error) {
     console.log(error);
+    res.status(500).json({ message: "Internal Server Error" })
+
   }
+})
+
+router.get("/locations/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const currentHost = await Host.findById(id).populate('jamSessions');
+    res.status(200).json(currentHost);
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: "Internal Server Error" })
+  }
+
 })
 
 module.exports = router;
